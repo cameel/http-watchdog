1714abbeb35a7b25f682481e68d3560377346f77
cameel <cameel2@gmail.com>
2013-08-12 05:14:42 +0200

    main.py: Extract logging configuration code into a separate method

--- main.py
+++ main.py
@@ -9,34 +9,33 @@ from settings_manager import SettingsManager, ConfigurationError
 
 logger = logging.getLogger(__name__)
 
-def configure_console_logging(level):
-    root_logger = logging.getLogger()
-
+def configure_console_logging(logger, level):
     formatter = logging.Formatter('%(message)s')
 
     handler = logging.StreamHandler()
     handler.setLevel(level)
     handler.setFormatter(formatter)
 
-    root_logger.addHandler(handler)
+    logger.addHandler(handler)
 
-def configure_file_logging(level, log_path):
-    root_logger = logging.getLogger()
-
+def configure_file_logging(logger, level, log_path):
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     handler = logging.FileHandler(log_path)
     handler.setLevel(level)
     handler.setFormatter(formatter)
 
-    root_logger.addHandler(handler)
+    logger.addHandler(handler)
 
-if __name__ == '__main__':
+def configure_logging():
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
 
-    configure_console_logging(logging.INFO)
-    configure_file_logging(logging.DEBUG, 'http_watchdog.log')
+    configure_console_logging(root_logger, logging.INFO)
+    configure_file_logging(root_logger, logging.DEBUG, 'http_watchdog.log')
+
+if __name__ == '__main__':
+    configure_logging()
 
     try:
         settings_manager = SettingsManager()
